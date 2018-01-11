b3776b44d85656dc38ccc21432b5b2c136906f66
liudenghui <liudenghui@51talk.com>
2018-01-11 17:29:33 +0800

    第一次引入common，然而并没有卵用

--- src/chunk2.js
+++ src/chunk2.js
@@ -1,6 +1,4 @@
 
-require('./chunk1');
-
 function chunk2(){
     console.log('i use the chunk1 one time...');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ module.exports = {
         }]
     },
     plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'chunk'
+        }),
         new HtmlWebpackPlugin({
             title: Date.now(),
             filename: 'ttt.html',
